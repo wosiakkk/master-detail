ec18fd840824748af1e910ee415988093edd96c4
Bruno Wosiak <brunofwosiak@gmail.com>
2020-03-29 20:05:12 -0300

    Adicionando estrutura Categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'categories',  loadChildren: () => import('./pages/categories/categories.module').then(m => m.CategoriesModule),}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
