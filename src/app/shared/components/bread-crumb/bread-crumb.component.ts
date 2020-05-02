177901a6e5ca107e37975601de0f22820f018a9a
Bruno Wosiak <brunofwosiak@gmail.com>
2020-05-02 19:56:39 -0300

    adding RouterModule to SharedModule && improving page components with BreadCrumbComponent

--- src/app/shared/components/bread-crumb/bread-crumb.component.ts
+++ src/app/shared/components/bread-crumb/bread-crumb.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+//interface para padronizar o tipo do item do breadcrumb
+interface BreadCrumbItem{
+  text: string;
+  link?: string; //link opcional, pois o item referente a page atual é somente active
+}
 
 @Component({
   selector: 'app-bread-crumb',
@@ -7,9 +13,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadCrumbComponent implements OnInit {
 
+  @Input() itens: Array<BreadCrumbItem> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+//método para verificar se o item é o último da lista, o qual receberá a classe active no template
+  isTheLastItem(item: BreadCrumbItem): boolean{
+    const index = this.itens.indexOf(item)
+    return index + 1 == this.itens.length;
   }
 
 }
